64f54a02e747dc1a91a63d3fc869e8b2933ef835
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2016-11-12 22:49:15 +0000

    .

--- Silent/Silent_Core/GL_GameSystem/GL_Game.cpp
+++ Silent/Silent_Core/GL_GameSystem/GL_Game.cpp
@@ -1,32 +1,100 @@
-#include "stdafx.h"
 #include "GL_Game.h"
 
 
 Silent_Core::GameSystem::GL_Game::GL_Game()
 {
+
+	// initialize SDL
+	SDL_Init(SDL_INIT_VIDEO);
+
+	if (m_windowType == "Borderless") {
+		m_borderType = SDL_WINDOW_BORDERLESS;
+	}
+
+	else if (m_windowType == "Fullscreen") {
+		m_borderType = SDL_WINDOW_FULLSCREEN;
+	}
+
+	else if (m_windowType == "Resizable") {
+		m_borderType = SDL_WINDOW_RESIZABLE;
+	}
+
+	else if (m_windowType == "Fullscreen Desktop") {
+		m_borderType = SDL_WINDOW_FULLSCREEN_DESKTOP;
+	}
+
+	else if (m_windowType == "Hidden") {
+		m_borderType = SDL_WINDOW_HIDDEN;
+	}
+
+	SDL_CreateWindow(
+		m_title,
+		m_windowPosX,
+		m_windowPosY,
+		m_windowWidth,
+		m_windowHeight,
+		SDL_WINDOW_OPENGL | m_borderType);
+
+	m_glContext = SDL_GL_CreateContext(m_screen);
+
+	setUpOpenGL();
+
+	SDL_GL_SetSwapInterval(1);
+
+#ifndef __APPLE__
+	glewExperimental = GL_TRUE;
+	glewInit();
+#endif
+
+	OnLoad();
+
+	while (m_running == true) {
+		OnUpdate();
+		OnRender();
+	}
+
+	OnClosing();
+	//Terminate SDL here or in OnClosing
+
+	OnClosed();
+}
+
+void Silent_Core::GameSystem::GL_Game::setUpOpenGL()
+{
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
 }
 
 void Silent_Core::GameSystem::GL_Game::OnLoad()
 {
-	throw gcnew System::NotImplementedException();
+	
+	OnLoadGame();
 }
 
 void Silent_Core::GameSystem::GL_Game::OnUpdate()
 {
-	throw gcnew System::NotImplementedException();
+	OnUpdateGame();
 }
 
 void Silent_Core::GameSystem::GL_Game::OnRender()
 {
-	throw gcnew System::NotImplementedException();
+	
+	OnRenderGame();
 }
 
 void Silent_Core::GameSystem::GL_Game::OnClosing()
 {
-	throw gcnew System::NotImplementedException();
+	
+	OnClosingGame();
 }
 
 void Silent_Core::GameSystem::GL_Game::OnClosed()
 {
-	throw gcnew System::NotImplementedException();
+	
+	OnClosedGame();
 }
--- Silent/Silent_Core/GL_GameSystem/GL_Game.h
+++ Silent/Silent_Core/GL_GameSystem/GL_Game.h
@@ -1,16 +1,32 @@
 #pragma once
 
+#include <GL\glew.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+
 namespace Silent_Core {
 	namespace GameSystem {
 		public ref class GL_Game
 		{
 		private:
-			char* title;
+			//SDL screen (window)
+			SDL_Window* m_screen;
+
+			//OpenGL context
+			SDL_GLContext m_glContext;
+
+			char* m_title;
+			char* m_windowType;
+			int m_borderType;
 
 			int m_windowWidth;
 			int m_windowHeight;
 			int m_windowPosX;
 			int m_windowPosY;
+
+			bool m_running = true;
+
+
 
 		public:
 			GL_Game();
@@ -22,6 +38,7 @@ namespace Silent_Core {
 			virtual void OnClosedGame() = 0;
 
 		private:
+			void setUpOpenGL();
 
 			void OnLoad();
 			void OnUpdate();
